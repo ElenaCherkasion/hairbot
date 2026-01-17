561f57bb0e55e88cc1a0f3ba0a0a0737b4d37e8f
ElenaCherkasion <cherkashina720@gmail.com>
2026-01-17 13:44:50 +1000

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,4 +1,4 @@
-import { отправитьСообщение } from '../../../utils/telegram-api.js';
+import { отправитьСообщение } from '../utils/telegram-api.js';
 import { getMainKeyboard } from '../keyboards/main.js';
 import { getWelcomeText } from '../utils/text-templates.js';
 
