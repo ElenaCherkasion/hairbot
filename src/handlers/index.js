c617e08b8b8ea26e55c02fe866842679d7f6621d
ElenaCherkasion <cherkashina720@gmail.com>
2026-01-17 12:44:00 +1000

    Update index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,1 +1,4 @@
-
+export { handleStart } from './start.js';
+export { handlePhoto } from './photo.js';
+export { handleCallback } from './callback.js';
+export { handleTariffSelection } from './tariff.js';
