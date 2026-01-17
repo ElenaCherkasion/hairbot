10d643d5353f2a2a88ce6e03c72eb6446ccd641d
ElenaCherkasion <cherkashina720@gmail.com>
2026-01-17 12:42:55 +1000

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,1 +1,10 @@
+import { отправитьСообщение } from '../../utils/telegram-api.js';
+import { getMainKeyboard } from '../keyboards/main.js';
+import { getWelcomeText } from '../utils/text-templates.js';
 
+export async function handleStart(userId, chatId, userData) {
+  const text = getWelcomeText();
+  const keyboard = getMainKeyboard();
+  
+  await отправитьСообщение(chatId, text, keyboard);
+}
