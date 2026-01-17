00722bc8db72650a699306456d8dd0bbaf509fc8
ElenaCherkasion <cherkashina720@gmail.com>
2026-01-17 13:11:55 +1000

    Update start.js

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,10 +1,5 @@
-import { отправитьСообщение } from '../../utils/telegram-api.js';
-import { getMainKeyboard } from '../keyboards/main.js';
-import { getWelcomeText } from '../utils/text-templates.js';
-
-export async function handleStart(userId, chatId, userData) {
-  const text = getWelcomeText();
-  const keyboard = getMainKeyboard();
-  
-  await отправитьСообщение(chatId, text, keyboard);
-}
+// Было: import { отправитьСообщение } from '../../utils/telegram-api.js';
+// Стало:
+import { отправитьСообщение } from '../../../utils/telegram-api.js';
+// Или если utils в корне:
+import { отправитьСообщение } from '../../../../utils/telegram-api.js';
