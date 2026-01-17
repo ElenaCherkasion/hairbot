e1b433afbba9b9cb51b21687ec83675fa2c2b4ad
ElenaCherkasion <cherkashina720@gmail.com>
2026-01-17 17:43:01 +1000

    Update index.js

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,4 +1,20 @@
-export { handleStart } from './start.js';
-export { handlePhoto } from './photo.js';
-export { handleCallback } from './callback.js';
-export { handleTariffSelection } from './tariff.js';
+// src/handlers/index.js
+import logger from '../utils/logger.js';
+
+export function setupWebhook(bot, url) {
+  return bot.telegram.setWebhook(url)
+    .then(() => {
+      logger.info(`Webhook установлен на ${url}`);
+      return true;
+    })
+    .catch(error => {
+      logger.error(`Ошибка установки webhook: ${error.message}`);
+      throw error;
+    });
+}
+
+// Экспортируем все обработчики
+export { default as startHandler } from './start.js';
+export { default as photoHandler } from './photo.js';
+export { default as tariffsHandler } from './tariffs.js';
+export { default as callbackHandler } from './callback.js';
