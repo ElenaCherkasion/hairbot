818735bc3368df32a5db78f5f25febede0a8c107
ElenaCherkasion <cherkashina720@gmail.com>
2026-01-17 12:43:39 +1000

    Update callback.js

--- src/handlers/callback.js
+++ src/handlers/callback.js
@@ -1,1 +1,36 @@
+import { отправитьСообщение } from '../../utils/telegram-api.js';
+import { handleStart } from './start.js';
+import { handleTariffSelection } from './tariff.js';
+import { getAboutText, getExamplesText, getTariffsText } from '../utils/text-templates.js';
+import { getMainKeyboard, getBackKeyboard } from '../keyboards/main.js';
 
+export async function handleCallback(userId, chatId, callbackData) {
+  switch(callbackData) {
+    case 'menu':
+      await handleStart(userId, chatId);
+      break;
+      
+    case 'about':
+      await отправитьСообщение(chatId, getAboutText(), getBackKeyboard());
+      break;
+      
+    case 'examples':
+      await отправитьСообщение(chatId, getExamplesText(), getBackKeyboard());
+      break;
+      
+    case 'tariffs':
+      await отправитьСообщение(chatId, getTariffsText(), getMainKeyboard());
+      break;
+      
+    case 'free':
+    case 'basic':
+    case 'pro':
+    case 'premium':
+      await handleTariffSelection(userId, chatId, callbackData);
+      break;
+      
+    default:
+      await отправитьСообщение(chatId, 'Неизвестная команда', getMainKeyboard());
+      break;
+  }
+}
